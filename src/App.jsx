4661ebaa886b4f8a799cefa071174092d75b7288
MAMCB <96302464+MAMCB@users.noreply.github.com>
2023-11-21 18:33:29 +0100

    made working character selection

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import "./App.css";
 function App() {
   const [characters, setCharacters] = useState([]);
   const [page, setPage] = useState(1);
+  const [currentCharacter,setCurrentCharacter] =useState(null);
   const lastPage = 42;
   useEffect(() => {
     fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
@@ -13,6 +14,9 @@ function App() {
       .then((data) => setCharacters(data.results))
       .catch((error) => console.error(error));
   }, [page]);
+    useEffect(()=>{
+      setCurrentCharacter(characters[0])
+    },[]);
   const nextPage = () => {
     setPage(page + 1);
   };
@@ -21,32 +25,65 @@ function App() {
     setPage(page - 1);
   };
 
+  const updateCharacter = (newCharacter) =>{
+    setCurrentCharacter(newCharacter);
+  }
+  const deselectCharacter = () =>{
+    setCurrentCharacter(null);
+  }
+
  
 
   return (
     <>
       <h1 id="top">The complete Rick and Morty character list</h1>
+
+      {currentCharacter ? (
+        <Character
+          key={uuidv4()}
+          character={currentCharacter}
+          clickable={false}
+          updateCharacter={updateCharacter}
+          image={currentCharacter.image}
+          name={currentCharacter.name}
+          status={currentCharacter.status}
+          gender={currentCharacter.gender}
+          type={currentCharacter.type}
+          species={currentCharacter.species}
+          origin={currentCharacter.origin.name}
+        />
+      ) : (
+        ""
+      )}
+
       <div className="grid-container">
-        {characters ? (
+        {!currentCharacter ? (
           characters.map((character) => (
             <Character
               key={uuidv4()}
+              character={character}
+              clickable={true}
+              updateCharacter={updateCharacter}
+              image={character.image}
               name={character.name}
-              picture={character.image}
-              species={character.species}
               status={character.status}
+              gender={character.gender}
+              type={character.type}
+              species={character.species}
               origin={character.origin.name}
             />
           ))
         ) : (
-          <p>No characters</p>
+          <button onClick={deselectCharacter}>Back</button>
         )}
       </div>
 
-      <p>Current page: {page}</p>
-      <a href="#top">Back to top</a>
-      {page > 1 ? <button onClick={previousPage}>Previous page</button> : ""}
-      {page < lastPage ? <button onClick={nextPage}>Next page</button> : ""}
+      <div style={!currentCharacter ?{display:"block"}:{display:"none"}}>
+        <p>Current page: {page}</p>
+        <a href="#top">Back to top</a>
+        {page > 1 ? <button onClick={previousPage}>Previous page</button> : ""}
+        {page < lastPage ? <button onClick={nextPage}>Next page</button> : ""}
+      </div>
     </>
   );
 }
--- src/Components/Character.jsx
+++ src/Components/Character.jsx
@@ -1,17 +1,43 @@
-const Character =({name,picture,species,status,origin})=>{
-return (
-  <>
-    <div className="grid-item">
-      <div className="character">
-        <img src={picture} alt="character picture" />
-        <div>
-          <h3>{name}</h3>
-          <p>{status}-{species}</p>
-          <p>Origin: {origin}</p>
+const Character = ({
+  character,
+  updateCharacter,
+  clickable,image,name,status,type,gender,origin,species
+}) => {
+  const handleCharacter = ()=>{
+    updateCharacter(character);
+  }
+
+  const noEffect=()=>{
+    return
+  }
+
+  return (
+    <>
+      <div className="grid-item">
+        <div
+          className="character"
+          style={!clickable ? { width: "50rem" } : { width: "25rem" }}
+          onClick={clickable ? handleCharacter : noEffect}
+        >
+          <img src={image} alt="character picture" />
+          <div>
+            <h3>{name}</h3>
+            <p>
+              {status}-{species}
+            </p>
+            {!clickable ? (
+              <p>
+                {type}-{gender}
+              </p>
+            ) : (
+              ""
+            )}
+
+            <p>Origin: {origin}</p>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
 };
 export default Character;
